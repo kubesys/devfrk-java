5a891018529a2c55dbf2b3d1105fb75303366590
Heng WU <wuheng@iscas.ac.cn>
2022-04-04 16:51:36 +0800

    init testfrk

--- devfrk/nuwa/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"io.github.kubesys.devfrk"},{"l":"io.github.kubesys.devfrk.annotation"},{"l":"io.github.kubesys.devfrk.configs"},{"l":"io.github.kubesys.devfrk.httpserver"},{"l":"io.github.kubesys.devfrk.httpserver.impl"},{"l":"io.github.kubesys.devfrk.mgr"},{"l":"io.github.kubesys.devfrk.utils"},{"l":"所有程序包","u":"allpackages-index.html"}];updateSearchResults();
