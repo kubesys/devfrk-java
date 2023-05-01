6817d87afb6f093161fc34c6ee8fbb3646967d06
syswu <wuheng@iscas.ac.cn>
2023-05-01 21:23:30 +0800

    update

---
+++ springboot/src/main/java/io/github/kubesys/devfrk/tools/annotations/Description.java
@@ -0,0 +1,34 @@
+/**
+ * Copyrigt (2019, ) Institute of Software, Chinese Academy of Sciences
+ */
+package io.github.kubesys.devfrk.tools.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wuheng@iscas.ac.cn
+ * @since  1.1.0
+ *
+ */
+@Target({FIELD}) 
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Description {
+
+	/**
+	 * The value may indicate a suggestion for a logical component name,
+	 * to be turned into a Spring bean in case of an auto-detected component.
+	 * @return the suggested component name, if any (or empty String otherwise)
+	 */
+	String desc() default "";
+	
+	String regexp() default "";
+}
